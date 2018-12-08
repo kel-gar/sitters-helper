7155308cb7d9e7d0822c3d0d0797528d0f85c511
Kelly Garry <kellygarry@KG-Macbook-Air.local>
2018-12-07 21:19:03 -0700

    styling

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Landing.css';
-import { Card,  CardImg, CardTitle, CardText, CardDeck, CardBody} from 'reactstrap';
+import { Button, Card,  CardImg, CardTitle, CardText, CardDeck, CardBody} from 'reactstrap';
 
 
 class Landing extends Component {
@@ -11,27 +11,30 @@ class Landing extends Component {
     <h1 className="hero-title">All of your important household Info in 1 place</h1>
       <CardDeck>
       <Card>
-        <CardImg top width="100%" src={process.env.PUBLIC_URL + '/family.jpg'} alt="logo" />
+        <CardImg top width="100%" src={process.env.PUBLIC_URL + '/family.jpg'} alt="family at the lake" />
         <CardBody>
-          <CardTitle>SHARE</CardTitle>
+          <CardTitle>SHARE <i class="fab fa-slideshare"></i></CardTitle>
           <CardText>With all your children's sitters and family members</CardText>
           <CardText>House Info, Emergency Contact Info, Medical Info, FUN Info!</CardText>
+          {/* <Button>Button</Button> */}
         </CardBody>
       </Card>
       <Card>
-        <CardImg top width="100%" src={process.env.PUBLIC_URL + '/stickys.jpg'} alt="logo" />
+        <CardImg top width="100%" src={process.env.PUBLIC_URL + '/stickys.jpg'} alt="picture of many sticky notes" />
         <CardBody>
-          <CardTitle>NO MORE STICKY NOTES!</CardTitle>
+          <CardTitle>NO MORE STICKY NOTES! <i class="far fa-sticky-note"></i></CardTitle>
           <CardText>Stop writing notes for every sitter, everytime</CardText>
           <CardText>Don't worry if you forgot anything, it's all online :)</CardText>
+          {/* <Button>Button</Button> */}
         </CardBody>
       </Card>
       <Card>
-        <CardImg top width="100%" src={process.env.PUBLIC_URL + '/computer.jpg'} alt="logo" />
+        <CardImg top width="100%" src={process.env.PUBLIC_URL + '/computer.jpg'} alt="desktop with keyboard and mouse" />
         <CardBody>
-          <CardTitle>Save Time!</CardTitle>
+          <CardTitle>SAVE TIME <i class="far fa-clock"></i></CardTitle>
           <CardText>Add or Edit sitter notes, anytime</CardText>
           <CardText>All from your desktop or mobile device</CardText>
+          {/* <Button>Button</Button> */}
         </CardBody>
       </Card>
     </CardDeck>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import {
-  Button,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledDropdown,
+  // Button,
+  // DropdownToggle,
+  // DropdownMenu,
+  // DropdownItem,
+  // UncontrolledDropdown,
   Collapse,
   Navbar,
   NavbarToggler,
@@ -50,7 +50,7 @@ class NavBar extends Component {
     return (
       <div>
         <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">Welcome to Sitter's Helper</NavbarBrand>
+          <NavbarBrand href="/">Welcome to Sitter's Helper <i class="fas fa-child"></i></NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
